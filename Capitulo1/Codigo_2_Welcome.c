79144bcc02faba89ea077c0abcd5b21c476b9595
Nodelay <121840258+Iraildo-Sousa@users.noreply.github.com>
2024-12-26 15:13:06 -0300

    Add files via upload

---
+++ Capitulo1/Codigo_2_Welcome.c
@@ -0,0 +1,13 @@
+/* Agora vamos entender o corpo código anterior */
+
+#include <stdio.h>
+
+/* A função main inicia a execução do programa */
+
+int main ( void ) {
+
+    printf("Bem vindo ao C!\n"); /* Comando a ser executado */
+
+    return 0; /* Indica que o programa terminou com sucesso */
+    
+} /* Fim da função main */
